4441c8a78d661b7e93c733092b9be5be4deabdf5
Harry <hlynch02@tufts.edu>
2026-02-23 19:42:07 -0500

    initial dotfiles

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,22 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.python3_host_prog = vim.fn.expand("/usr/bin/python3")
+vim.opt.clipboard = "unnamedplus"
+
+vim.g.clipboard = {
+  name = "win32yank",
+  copy = {
+    ["+"] = "win32yank -i --crlf",
+    ["*"] = "win32yank -i --crlf",
+  },
+  paste = {
+    ["+"] = "win32yank -o --lf",
+    ["*"] = "win32yank -o --lf",
+  },
+  cache_enabled = 0,
+}
---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin", lazy = false },
+  { "rose-pine/neovim", name = "rose-pine", lazy = false },
+  { "ellisonleao/gruvbox.nvim", lazy = false },
+  { "rebelot/kanagawa.nvim", lazy = false },
+  { "EdenEast/nightfox.nvim", lazy = false },
+  { "marko-cerovac/material.nvim", lazy = false },
+  { "sainnhe/everforest", lazy = false },
+  { "arcticicestudio/nord-vim", lazy = false },
+  { "dracula/vim", name = "dracula", lazy = false },
+  { "folke/tokyonight.nvim", lazy = false },
+}
